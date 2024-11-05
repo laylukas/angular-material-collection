861f8686739fb9c834e7628faa1966f49bf7bbf0
SebiKay95 <sebi.kiefer@googlemail.com>
2024-11-05 12:44:46 +0100

    okay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './material-components/toolbar/toolbar.component';
+import { SidebarComponent } from './material-components/sidebar/sidebar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, ToolbarComponent],
+  imports: [RouterOutlet, CommonModule, ToolbarComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/material-components/toolbar/toolbar.component.ts
+++ src/app/material-components/toolbar/toolbar.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
   imports: [
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule,
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
