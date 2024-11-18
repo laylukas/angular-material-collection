e87521baa4707bb5667c7d5ee061ace78b7357d4
SebiKay95 <sebi.kiefer@googlemail.com>
2024-11-18 22:44:40 +0100

    error: fix sidbar import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './material-components/toolbar/toolbar.component';
-import { SidebarComponent } from './material-components/sidebar/sidebar.component';
-
 
 @Component({
   selector: 'app-root',
